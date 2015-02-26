0ac3cbca0db3ba5a1381b0656848e31869d8787d
Marco don Kaulea <DonMarco42@gmail.com>
2015-02-26 22:30:18 +0100

    First implementation

--- kraulbot.go
+++ kraulbot.go
@@ -6,3 +6,32 @@
 // Marco 'don' Kaulea
 // ----------------------------------------------------------------------------
 package main
+
+import "fmt"
+import "strings"
+import "github.com/thoj/go-ircevent"
+
+func main() {
+	con := irc.IRC("kraulbot", "kraulbot")
+	err := con.Connect("irc.hackint.eu:6669")
+	if err != nil {
+		fmt.Println("Failed connecting")
+		fmt.Println(err)
+		return
+	}
+	con.AddCallback("001", func(e *irc.Event) {
+		con.Join("#kraulbot")
+	})
+	con.AddCallback("PRIVMSG", func(e *irc.Event) {
+		message := e.Message()
+		fmt.Println(message)
+		if !strings.HasPrefix(message, "kraulbot") {
+			return
+		}
+		if !strings.Contains(strings.ToLower(message), "miau") {
+			return
+		}
+		con.Privmsg("#kraulbot", fmt.Sprintf("%s *kraul*", e.Nick))
+	})
+	con.Loop()
+}
